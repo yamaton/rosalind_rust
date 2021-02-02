65dd1c5a370a43ec235b0c08f354d0d3dd6e07e0
yamaton <yamaton@gmail.com>
2021-02-01 19:58:00 -0500

    Add ba1c

---
+++ src/bin/ba1c.rs
@@ -0,0 +1,51 @@
+/*
+http://rosalind.info/problems/ba1c/
+
+Find the reverse complement of a string
+
+Sample:
+AAAACCCGGT
+
+ACCGGGTTTT
+
+*/
+
+extern crate rosalind_rust;
+use std::io::{prelude::*, BufReader};
+use structopt::StructOpt;
+
+fn revcomp(s: &str) -> String {
+    s.chars()
+        .map(|c| match c {
+            'A' => 'T',
+            'T' => 'A',
+            'G' => 'C',
+            'C' => 'G',
+            _ => 'N',
+        })
+        .rev()
+        .collect::<String>()
+}
+
+#[test]
+fn test_ba1c() {
+    let seq = String::from("AAAACCCGGT");
+    assert_eq!(revcomp(&seq), String::from("ACCGGGTTTT"));
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::from_args();
+    let f = std::fs::File::open(&args.path)?;
+    let reader = BufReader::new(f);
+    let mut iter = reader.lines();
+    let text = iter.next().unwrap().unwrap();
+    let res = revcomp(&text);
+    println!("{}", res);
+    Ok(())
+}
